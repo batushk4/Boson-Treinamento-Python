e1f7d9463333f3a5f2a01449ac7263dc095539be
barfly <oseias.laborda@gmail.com>
2024-01-06 23:07:59 -0400

    variaveis

---
+++ 1.variaveis.py
@@ -0,0 +1,19 @@
+nome = 'Bêbado'
+print(nome)
+
+media = 0
+n1 = n2 = n3 = n4 = 0.0
+
+nome, idade = "Bebo", 23
+print(nome, idade)
+
+# Função type()
+print(type(media))
+
+# Função ininstance()
+a = 10
+b = "sol"
+
+print(isinstance(a, int))
+print(isinstance(b, str))
+print(isinstance(a, (float, int)))
